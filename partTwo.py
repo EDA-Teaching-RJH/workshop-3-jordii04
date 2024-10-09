e3699243ccbc3321e16104f4c6cc58ea44f0515d
jordii04 <jrq2@kent.ac.uk>
2024-10-09 11:48:58 +0000

    W3 Part 2

--- partTwo.py
+++ partTwo.py
@@ -1,1 +1,21 @@
 
+#Program to generate a random number between (1, 10)
+#import the random module#
+
+import random
+secret_number = random.randint(1, 10)
+guess = int(input("Enter any number! "))
+secret_number= 6
+while secret_number!= guess:
+    if guess < secret_number: print("Too low") 
+    elif guess > secret_number: print("Too high")
+    guess = print("Try Again")
+    guess = int(input("Enter any number again! "))
+guess = print("Try Again")
+guess = int(input("Enter any number again! ")) 
+if guess := secret_number:  print ("You guessed correctly")
+
+
+ 
+
+
