af61e25f4c7be61f6d024617842c640525d23ebf
jordii04 <jrq2@kent.ac.uk>
2024-10-09 10:42:22 +0000

    main

--- partOne.py
+++ partOne.py
@@ -1,1 +1,8 @@
 
+print ("What's your age?")
+age = int(input("Enter age:"))
+#check if age is greater or smaller than 17
+if age < 17:
+    print ("You are a child")
+if age > 17:
+    print ("You are an adult.") 
