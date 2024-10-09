5bf2202c96dca135195e0309e05217491d74f33e
jordii04 <jrq2@kent.ac.uk>
2024-10-09 12:26:56 +0000

    W3 part1

--- partTwo.py
+++ partTwo.py
@@ -5,7 +5,7 @@
 import random
 secret_number = random.randint(1, 10)
 guess = int(input("Enter any number! "))
-secret_number= 6
+
 while secret_number!= guess:
     if guess < secret_number: print("Too low") 
     elif guess > secret_number: print("Too high")
